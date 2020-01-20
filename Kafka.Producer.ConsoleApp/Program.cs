d5a2dcf9c43d36f60c2d5c9e31b260d66af746d1
Rafael Luis da Costa Coelho <rafael.coelho@autoglass.com.br>
2020-01-20 16:56:19 -0300

    Add the kafka producer console application.

---
+++ Kafka.Producer.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+namespace Kafka.Producer.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            string command = "";
+
+            while (command.ToLower() != "exit")
+            {
+                Console.Write("Mensagem: ");
+
+                command = Console.ReadLine();
+
+                using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+                    try
+                    {
+                        var dr = await producer.ProduceAsync("chat-topic", new Message<Null, string> { Value = command });
+
+                        Console.WriteLine($"Mensagem '{dr.Value}' entregue para '{dr.TopicPartitionOffset}'");
+                    }
+                    catch (ProduceException<Null, string> exception)
+                    {
+                        Console.WriteLine($"Falha na entrega! {exception.Error.Reason}");
+                    }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# kafka-producer-console-app
+# Kafka Producer Console App
+
+The kafka producer in dotnet core console application.
